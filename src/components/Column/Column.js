d2f36c6520a792f9f6c87314316e127d9948842e
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-04-15 13:21:35 +0200

    Added columns and cards reducer (12.4)

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
-// import Creator from './../Creator/Creator.js';
-// import { settings } from '../../data/dataStore';
+import Creator from './../Creator/Creator.js';
+import { settings } from '../../data/dataStore';
 import Icon from './../Icon /Icon.js';
 
 
@@ -12,10 +12,18 @@ class Column extends React.Component {
       title: PropTypes.node.isRequired,
       cards: PropTypes.array,
       icon: PropTypes.node,
+      addCard: PropTypes.func,
+    }
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
+    }
+
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
     }
     
     render(){
-      const {title, icon, cards} = this.props;
+      const {title, icon, cards, addCard} = this.props;
       return(
         <section className={styles.component}>
           <h3 className={styles.title}><span className={styles.icon}><Icon name={icon}/></span>{title}</h3>
@@ -26,11 +34,11 @@ class Column extends React.Component {
             ))}
           </div>
                 
-          {/*
+          
           <div>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+            <Creator text={settings.cardCreatorText} action={addCard} />
           </div>
-          */}
+          
         </section>
       );        
     }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import Column from './../Column/ColumnContainer.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-// import Creator from './../Creator/Creator.js';
+import Creator from './../Creator/Creator.js';
 
 class List extends React.Component {
   static propTypes = {
@@ -13,6 +13,7 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.node,
+    addColumn: PropTypes.func,
   }
   static defaultProps = {
     description: settings.defaultListDescription,
@@ -20,7 +21,7 @@ class List extends React.Component {
   }
 
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
 
@@ -37,11 +38,11 @@ class List extends React.Component {
           ))}
         </div>
 
-        {/*
+      
         <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          <Creator text={settings.columnCreatorText} action={addColumn}/>
         </div>
-        */}
+        
 
       </section>
     );
